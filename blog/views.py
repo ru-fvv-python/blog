c0b68ca65dc55daecfb358956aafa00f17b19688
vlad-fvv <vladfomin2008@gmail.com>
2023-07-10 12:17:57 +0300

    четвертиый коммит. Созданы шаблоны, css, представления, роуты для отображения  блогов списком и по одному.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from blog.models import Post
+
 
 # Create your views here.
+# данном представлении извлекаются все посты со статусом PUBLISHED
+def post_list(request):
+    posts = Post.published.all()
+    return render(request,
+                  'blog/post/list.html',
+                  {'posts': posts})
+
+
+# редставление одиночного поста на странице
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post})
